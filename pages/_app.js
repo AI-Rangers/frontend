343780d3e2aa14ae6d205f9b45a70c47c83f1c32
puff <puff.tw@gmail.com>
2022-12-05 07:12:22 +0800

    test liff

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,25 @@ import { useState, useEffect } from "react";
 function MyApp({ Component, pageProps }) {
   const [liffObject, setLiffObject] = useState(null);
   const [liffError, setLiffError] = useState(null);
+  // process.env.LIFF_ID
+  const LIFF_ID = process.env.NEXT_PUBLIC_LIFF_ID
 
   // Execute liff.init() when the app is initialized
   useEffect(() => {
     // to avoid `window is not defined` error
     import("@line/liff").then((liff) => {
       console.log("start liff.init()...");
+
       liff
-        .init({ liffId: process.env.LIFF_ID })
+        .init({ liffId: LIFF_ID })
         .then(() => {
           console.log("liff.init() done");
-          console.log("LIFF_ID = ", process.env.LIFF_ID);
+          console.log("LIFF_ID = ", LIFF_ID);
           setLiffObject(liff);
         })
         .catch((error) => {
           console.log(`liff.init() failed: ${error}`);
-          if (!process.env.LIFF_ID) {
+          if (!LIFF_ID) {
             console.info(
               "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
             );
