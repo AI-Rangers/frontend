9edebe13099e6427e5463c6cbb4c6a1d6afd8783
puff <puff.tw@gmail.com>
2022-12-09 05:53:42 +0800

    update

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Header() {
+
+  return (
+    <div>
+      <Head>
+        <title></title>
+      </Head>
+    </div>
+  );
+}
--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ export default function Document() {
         <NextScript />
         <script src="https://public.tableau.com/javascripts/api/tableau-2.min.js"></script>
         <script type="module" src="https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js"></script>
+        {/* <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script> */}
       </body>
     </Html>
   )
